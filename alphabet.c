f1d61d8fdd9bcc9d153d741ccbeec0185a5da8df
kanigayu <31438605+kanigayu@users.noreply.github.com>
2018-01-19 10:56:00 +0530

    alphabet.c
    alphabet

--- alphabet.c
+++ alphabet.c
@@ -4,7 +4,7 @@ int main()
 char ch;
 printf("Enter the character");
 scanf("%c",&ch);
-if((ch>='a'&&ch<='z')||(ch>='A'&&c<='Z'))
+if((ch>='a'&&ch<='z')||(ch>='A'&&ch<='Z'))
 {
 printf("The character is alphabet");
 }
